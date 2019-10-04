7d8ee09ba82a38b76f8603329463e641679b6633
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-10-04 19:17:45 +0700

    List Movie - Add ListView layout

--- app/src/main/java/io/github/fatimazza/moviecatalogue/ListMovieActivity.kt
+++ app/src/main/java/io/github/fatimazza/moviecatalogue/ListMovieActivity.kt
@@ -2,8 +2,13 @@ package io.github.fatimazza.moviecatalogue
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ListView
+import kotlinx.android.synthetic.main.activity_list_movie.*
 
 class ListMovieActivity : AppCompatActivity() {
+
+    private val listMovie: ListView
+    get() = lv_movie
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
