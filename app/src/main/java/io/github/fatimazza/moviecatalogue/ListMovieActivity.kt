820fe7e11b95aa1fda92def90531d568f4619e12
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-10-04 18:02:44 +0700

    Movie Catalogue - Initial Commit

---
+++ app/src/main/java/io/github/fatimazza/moviecatalogue/ListMovieActivity.kt
@@ -0,0 +1,12 @@
+package io.github.fatimazza.moviecatalogue
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListMovieActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_movie)
+    }
+}
