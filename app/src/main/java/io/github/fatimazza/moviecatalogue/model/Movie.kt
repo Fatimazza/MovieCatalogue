0d2f2dbba44c1361a988aac6029c07537106f770
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-10-04 21:10:39 +0700

    Movie Data - Add Data Class

---
+++ app/src/main/java/io/github/fatimazza/moviecatalogue/model/Movie.kt
@@ -0,0 +1,9 @@
+package io.github.fatimazza.moviecatalogue.model
+
+data class Movie(
+    var title: String = "",
+    var poster: String = "",
+    var description: String = "",
+    var releaseDate: String = "",
+    var runtime: String = ""
+)
