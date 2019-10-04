070a9e7c32c73e5efbfa64f98c072db0c008f491
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-10-04 21:59:29 +0700

    List Movie - Show List in Activity

--- app/src/main/java/io/github/fatimazza/moviecatalogue/ListMovieActivity.kt
+++ app/src/main/java/io/github/fatimazza/moviecatalogue/ListMovieActivity.kt
@@ -1,8 +1,11 @@
 package io.github.fatimazza.moviecatalogue
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ListView
+import io.github.fatimazza.moviecatalogue.model.Movie
+import io.github.fatimazza.moviecatalogue.model.MoviesData
 import kotlinx.android.synthetic.main.activity_list_movie.*
 
 class ListMovieActivity : AppCompatActivity() {
@@ -10,8 +13,19 @@ class ListMovieActivity : AppCompatActivity() {
     private val listMovie: ListView
     get() = lv_movie
 
+    private var list: ArrayList<Movie> = arrayListOf()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_movie)
+
+        setupListMovieAdapter(this)
+    }
+
+    private fun setupListMovieAdapter(context: Context) {
+        list.addAll(MoviesData.listData)
+
+        val adapter = ListMovieAdapter(context, list)
+        listMovie.adapter = adapter
     }
 }
