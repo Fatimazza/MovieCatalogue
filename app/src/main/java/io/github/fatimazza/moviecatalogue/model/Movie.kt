63cad2eac288b5e4fd9a24cc4a23abb6be921c38
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-10-04 22:14:28 +0700

    Movie Data - Make Data Class Parcelable

--- app/src/main/java/io/github/fatimazza/moviecatalogue/model/Movie.kt
+++ app/src/main/java/io/github/fatimazza/moviecatalogue/model/Movie.kt
@@ -1,9 +1,13 @@
 package io.github.fatimazza.moviecatalogue.model
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class Movie(
     var title: String = "",
     var poster: Int = 0,
     var description: String = "",
     var releaseDate: String = "",
     var runtime: String = ""
-)
+): Parcelable
