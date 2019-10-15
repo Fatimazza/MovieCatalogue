d8bfb7e56d410846236b639a4b674ff5ec567fad
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-10-15 13:39:02 +0700

    Bottom Navigation - Add Listener for selected menu

--- app/src/main/java/io/github/fatimazza/moviecatalogue/MainActivity.kt
+++ app/src/main/java/io/github/fatimazza/moviecatalogue/MainActivity.kt
@@ -1,12 +1,35 @@
 package io.github.fatimazza.moviecatalogue
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val bottomNavigation: BottomNavigationView
+        get() = nav_view_main
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomNavigation.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
     }
+
+    private val onNavigationItemSelectedListener =
+        object : BottomNavigationView.OnNavigationItemSelectedListener {
+            override fun onNavigationItemSelected(item: MenuItem): Boolean {
+                when (item.itemId) {
+                    R.id.navigation_movie -> {
+                        return true
+                    }
+                    R.id.navigation_tvshow -> {
+                        return true
+                    }
+                }
+                return false
+            }
+        }
 }
