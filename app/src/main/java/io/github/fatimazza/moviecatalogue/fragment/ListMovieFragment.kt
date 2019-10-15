b4f541bfc13da2c40d645bb26446f10307c5e0b0
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-10-15 14:14:17 +0700

    List Movie - Show List Movie in Fragment

--- app/src/main/java/io/github/fatimazza/moviecatalogue/fragment/ListMovieFragment.kt
+++ app/src/main/java/io/github/fatimazza/moviecatalogue/fragment/ListMovieFragment.kt
@@ -5,10 +5,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ListView
 import androidx.fragment.app.Fragment
+import io.github.fatimazza.moviecatalogue.ListMovieAdapter
 import io.github.fatimazza.moviecatalogue.R
+import io.github.fatimazza.moviecatalogue.model.Movie
+import kotlinx.android.synthetic.main.activity_list_movie.*
 
 class ListMovieFragment : Fragment() {
+
+    private val listMovie: ListView
+        get() = lv_movie
+
+    private var list: ArrayList<Movie> = arrayListOf()
+
+    private lateinit var listMovieAdapter: ListMovieAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,4 +29,37 @@ class ListMovieFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_list_movie, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupListMovieAdapter()
+    }
+
+    private fun setupListMovieAdapter() {
+        list.addAll(getMoviesData())
+
+        listMovieAdapter = ListMovieAdapter(requireContext(), list)
+        listMovie.adapter = listMovieAdapter
+    }
+
+    private fun getMoviesData(): ArrayList<Movie> {
+        val movieTitle = resources.getStringArray(R.array.movie_title)
+        val moviePoster = resources.obtainTypedArray(R.array.movie_poster)
+        val movieDesc = resources.getStringArray(R.array.movie_desc)
+        val movieRelease = resources.getStringArray(R.array.movie_release)
+        val movieRuntime = resources.getStringArray(R.array.movie_runtime)
+
+        val listMovie = ArrayList<Movie>()
+        for (position in movieTitle.indices) {
+            val movie = Movie(
+                movieTitle[position],
+                moviePoster.getResourceId(position, -1),
+                movieDesc[position],
+                movieRelease[position],
+                movieRuntime[position]
+            )
+            listMovie.add(movie)
+        }
+        moviePoster.recycle()
+        return listMovie
+    }
 }
