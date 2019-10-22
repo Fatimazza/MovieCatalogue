c5aced29f4c0c6f3ffc7a0a4afa88c949ea624b8
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-10-22 16:12:31 +0700

    Locale Change - Add initial Intent Service

---
+++ app/src/main/java/io/github/fatimazza/moviecatalogue/service/LocaleChangedService.kt
@@ -0,0 +1,11 @@
+package io.github.fatimazza.moviecatalogue.service
+
+import android.app.IntentService
+import android.content.Intent
+
+class LocaleChangedService : IntentService("LocaleChangedService") {
+
+    override fun onHandleIntent(intent: Intent?) {
+
+    }
+}
