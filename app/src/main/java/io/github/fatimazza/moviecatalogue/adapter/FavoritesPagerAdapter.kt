8d8542f71c966906421c6d32320132184f9f67aa
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-10-29 14:47:27 +0700

    Tab Favorites - Change Favorite Pager Adapter

--- app/src/main/java/io/github/fatimazza/moviecatalogue/adapter/FavoritesPagerAdapter.kt
+++ app/src/main/java/io/github/fatimazza/moviecatalogue/adapter/FavoritesPagerAdapter.kt
@@ -5,20 +5,20 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 import io.github.fatimazza.moviecatalogue.R
-import io.github.fatimazza.moviecatalogue.fragment.ListMovieFragment
-import io.github.fatimazza.moviecatalogue.fragment.ListTelevisionFragment
+import io.github.fatimazza.moviecatalogue.fragment.FavoriteMovieFragment
+import io.github.fatimazza.moviecatalogue.fragment.FavoriteTelevisionFragment
 
 class FavoritesPagerAdapter(fm: FragmentManager, context: Context) :
-    FragmentPagerAdapter(fm, FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+    FragmentPagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
 
     private val numberOfTab: Int = 2
     private var ctx: Context = context
 
     override fun getItem(position: Int): Fragment {
         return when (position) {
-            0 -> ListMovieFragment()
+            0 -> FavoriteMovieFragment()
             else -> {
-                return ListTelevisionFragment()
+                return FavoriteTelevisionFragment()
             }
         }
     }
