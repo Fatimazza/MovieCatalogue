8d2787276dab6c4b5c6dcde0061bd50df3dcd8b8
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-10-29 15:47:03 +0700

    Favorite Tv Show - Add Adapter, Show list in Activity

--- app/src/main/java/io/github/fatimazza/moviecatalogue/fragment/FavoriteTelevisionFragment.kt
+++ app/src/main/java/io/github/fatimazza/moviecatalogue/fragment/FavoriteTelevisionFragment.kt
@@ -5,10 +5,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import io.github.fatimazza.moviecatalogue.R
+import io.github.fatimazza.moviecatalogue.adapter.FavoriteTelevisionAdapter
+import kotlinx.android.synthetic.main.fragment_favorite_television.*
 
 
 class FavoriteTelevisionFragment : Fragment() {
+
+    private val listFavTelevision: RecyclerView
+        get() = rv_fav_tv
+
+    private lateinit var listFavTelevisionAdapter: FavoriteTelevisionAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -16,5 +25,17 @@ class FavoriteTelevisionFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_favorite_television, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupListFavoriteTelevisionAdapter()
+    }
+
+    private fun setupListFavoriteTelevisionAdapter() {
+        listFavTelevision.layoutManager = LinearLayoutManager(requireContext())
+        listFavTelevisionAdapter = FavoriteTelevisionAdapter()
+        listFavTelevisionAdapter.notifyDataSetChanged()
+        listFavTelevision.adapter = listFavTelevisionAdapter
     }
 }
