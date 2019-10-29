8706eb90520e0612bb78f2a25a14764eaf3eec7f
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-10-29 15:23:13 +0700

    Favorite Movie - Add Adapter, Show list in Activity

--- app/src/main/java/io/github/fatimazza/moviecatalogue/fragment/FavoriteMovieFragment.kt
+++ app/src/main/java/io/github/fatimazza/moviecatalogue/fragment/FavoriteMovieFragment.kt
@@ -5,10 +5,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import io.github.fatimazza.moviecatalogue.R
+import io.github.fatimazza.moviecatalogue.adapter.FavoriteMovieAdapter
+import kotlinx.android.synthetic.main.fragment_favorite_movie.*
 
 
 class FavoriteMovieFragment : Fragment() {
+
+    private val listFavMovie: RecyclerView
+        get() = rv_fav_movie
+
+    private lateinit var listFavMovieAdapter: FavoriteMovieAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,4 +27,15 @@ class FavoriteMovieFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_favorite_movie, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupListFavoriteMovieAdapter()
+    }
+
+    private fun setupListFavoriteMovieAdapter() {
+        listFavMovie.layoutManager = LinearLayoutManager(requireContext())
+        listFavMovieAdapter = FavoriteMovieAdapter()
+        listFavMovieAdapter.notifyDataSetChanged()
+        listFavMovie.adapter = listFavMovieAdapter
+    }
 }
