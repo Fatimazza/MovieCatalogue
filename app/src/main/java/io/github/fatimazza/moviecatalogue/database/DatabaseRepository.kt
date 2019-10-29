79081b3ce64ab25ac38ce56582c7e517a402b338
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-10-29 12:00:54 +0700

    Favorite Database - Add Database Repository, Add View Model

---
+++ app/src/main/java/io/github/fatimazza/moviecatalogue/database/DatabaseRepository.kt
@@ -0,0 +1,12 @@
+package io.github.fatimazza.moviecatalogue.database
+
+import androidx.lifecycle.LiveData
+
+class DatabaseRepository(private val favMovieDao: FavoriteDatabaseDao) {
+
+    val allFavMovies: LiveData<List<FavoriteMovie>> = favMovieDao.getAllMovies()
+
+    suspend fun insertMovie(movie: FavoriteMovie) {
+        favMovieDao.insertMovie(movie)
+    }
+}
---
+++ app/src/main/java/io/github/fatimazza/moviecatalogue/viewmodel/FavoriteViewModel.kt
@@ -0,0 +1,35 @@
+package io.github.fatimazza.moviecatalogue.viewmodel
+
+import android.app.Application
+import android.util.Log
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import io.github.fatimazza.moviecatalogue.database.DatabaseRepository
+import io.github.fatimazza.moviecatalogue.database.FavoriteDatabase
+import io.github.fatimazza.moviecatalogue.database.FavoriteMovie
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
+
+class FavoriteViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val repository: DatabaseRepository
+    val allFavMovies: LiveData<List<FavoriteMovie>>
+
+    init {
+        val favMovieDao = FavoriteDatabase.getInstance(application).favoriteDatabaseDao
+        repository = DatabaseRepository(favMovieDao)
+        allFavMovies = repository.allFavMovies
+    }
+
+    fun getAllFavoriteMovies(): LiveData<List<FavoriteMovie>> {
+        return allFavMovies
+    }
+
+    fun insertMovie(movie: FavoriteMovie) {
+        GlobalScope.launch {
+            repository.insertMovie(movie)
+            Log.d("Izza", "Movie ${movie.movieTitle} - - INSERTED")
+        }
+    }
+}
