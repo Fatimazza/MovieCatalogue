2186dc8ad1ea1f8eb29a25e5bdc13549933a8f3d
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-10-29 16:09:41 +0700

    Favorite Detail - Add Activity with needed Layout

---
+++ app/src/main/java/io/github/fatimazza/moviecatalogue/DetailFavoriteActivity.kt
@@ -0,0 +1,12 @@
+package io.github.fatimazza.moviecatalogue
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailFavoriteActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_favorite)
+    }
+}
