4c06fad105485897297930e0f45802c318b0f8f0
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-11-07 08:10:01 +0700

    Favorite Detail - Avoid null from Movie and Tv Show Response

--- app/src/main/java/io/github/fatimazza/moviecatalogue/DetailMovieActivity.kt
+++ app/src/main/java/io/github/fatimazza/moviecatalogue/DetailMovieActivity.kt
@@ -225,11 +225,11 @@ class DetailMovieActivity : AppCompatActivity() {
                     showLoading(false)
                     if (movieDetail != null) {
                         populateDetails(
-                            movieDetail.title,
-                            movieDetail.overview,
-                            movieDetail.release_date,
-                            movieDetail.vote_average.toString(),
-                            movieDetail.poster_path
+                            movieDetail.title ?: "",
+                            movieDetail.overview ?: "",
+                            movieDetail.release_date ?: "",
+                            movieDetail.vote_average.toString() ?: "",
+                            movieDetail.poster_path ?: ""
                         )
                         showDetailMovie(true)
                         setFavorite()
@@ -245,11 +245,11 @@ class DetailMovieActivity : AppCompatActivity() {
                     showLoading(false)
                     if (tvDetail != null) {
                         populateDetails(
-                            tvDetail.name,
-                            tvDetail.overview,
-                            tvDetail.first_air_date,
-                            tvDetail.vote_average.toString(),
-                            tvDetail.poster_path
+                            tvDetail.name ?: "",
+                            tvDetail.overview ?: "",
+                            tvDetail.first_air_date ?: "",
+                            tvDetail.vote_average.toString() ?: "",
+                            tvDetail.poster_path ?: ""
                         )
                         showDetailMovie(true)
                         setFavorite()
