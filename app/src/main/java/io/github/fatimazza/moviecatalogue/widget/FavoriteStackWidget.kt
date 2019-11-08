5f5c1e689fea98fb76cd890bbb769abb4341553f
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-11-08 16:40:08 +0700

    Favorite Stack Widget - Add initial Stack Widget and its generated files

---
+++ app/src/main/java/io/github/fatimazza/moviecatalogue/widget/FavoriteStackWidget.kt
@@ -0,0 +1,51 @@
+package io.github.fatimazza.moviecatalogue.widget
+
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.widget.RemoteViews
+
+import io.github.fatimazza.moviecatalogue.R
+
+/**
+ * Implementation of App Widget functionality.
+ */
+class FavoriteStackWidget : AppWidgetProvider() {
+
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+        // There may be multiple widgets active, so update all of them
+        for (appWidgetId in appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId)
+        }
+    }
+
+    override fun onEnabled(context: Context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    override fun onDisabled(context: Context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+
+    companion object {
+
+        internal fun updateAppWidget(
+            context: Context, appWidgetManager: AppWidgetManager,
+            appWidgetId: Int
+        ) {
+
+            val widgetText = context.getString(R.string.widget_fav_title)
+            // Construct the RemoteViews object
+            val views = RemoteViews(context.packageName, R.layout.favorite_stack_widget)
+            views.setTextViewText(R.id.empty_view, widgetText)
+
+            // Instruct the widget manager to update the widget
+            appWidgetManager.updateAppWidget(appWidgetId, views)
+        }
+    }
+}
+
