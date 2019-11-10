b28af06f3588bbe97842e5e32bad2beff1926297
Fatima Az <azzahro.fatima@gmail.com>
2019-11-11 06:54:04 +0700

    Daily Reminder - Add initial Broadcast Receiver

---
+++ app/src/main/java/io/github/fatimazza/moviecatalogue/receiver/DailyReminderAlarmReceiver.kt
@@ -0,0 +1,13 @@
+package io.github.fatimazza.moviecatalogue.receiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class DailyReminderAlarmReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        // This method is called when the BroadcastReceiver is receiving an Intent broadcast.
+        TODO("DailyReminderAlarmReceiver.onReceive() is not implemented")
+    }
+}
