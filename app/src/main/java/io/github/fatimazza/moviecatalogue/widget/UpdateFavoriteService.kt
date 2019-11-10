b81c2eabe4be26b0bb6a3ddca7d1f0c9b26c52ae
Fatima Az <azzahro.fatima@gmail.com>
2019-11-10 08:10:21 +0700

    Update Favorite Widget - Add Update Favorite Service

---
+++ app/src/main/java/io/github/fatimazza/moviecatalogue/widget/UpdateFavoriteService.kt
@@ -0,0 +1,16 @@
+package io.github.fatimazza.moviecatalogue.widget
+
+import android.app.job.JobParameters
+import android.app.job.JobService
+
+class UpdateFavoriteService : JobService() {
+
+    override fun onStopJob(jobParameters: JobParameters): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onStartJob(jobParameters: JobParameters): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
