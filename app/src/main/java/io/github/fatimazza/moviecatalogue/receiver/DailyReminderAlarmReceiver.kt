ce2404909d3cf55e1e25c127ccedc63bf3c305a8
Fatima Az <azzahro.fatima@gmail.com>
2019-11-11 06:56:45 +0700

    Daily Reminder - Implement onReceive function in Alarm Receiver

--- app/src/main/java/io/github/fatimazza/moviecatalogue/receiver/DailyReminderAlarmReceiver.kt
+++ app/src/main/java/io/github/fatimazza/moviecatalogue/receiver/DailyReminderAlarmReceiver.kt
@@ -3,11 +3,34 @@ package io.github.fatimazza.moviecatalogue.receiver
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.widget.Toast
 
 class DailyReminderAlarmReceiver : BroadcastReceiver() {
 
+    companion object {
+        const val TYPE_ONE_TIME = "OneTimeAlarm"
+        const val TYPE_REPEATING = "RepeatingAlarm"
+        const val EXTRA_MESSAGE = "message"
+        const val EXTRA_TYPE = "type"
+        // Siapkan 2 id untuk 2 macam alarm, onetime dan repeating
+        private const val ID_ONETIME = 100
+        private const val ID_REPEATING = 101
+    }
+
     override fun onReceive(context: Context, intent: Intent) {
         // This method is called when the BroadcastReceiver is receiving an Intent broadcast.
-        TODO("DailyReminderAlarmReceiver.onReceive() is not implemented")
+
+        val type = intent.getStringExtra(EXTRA_TYPE)
+        val message = intent.getStringExtra(EXTRA_MESSAGE)
+        val title =
+            if (type.equals(TYPE_ONE_TIME, ignoreCase = true)) TYPE_ONE_TIME else TYPE_REPEATING
+        val notifId =
+            if (type.equals(TYPE_ONE_TIME, ignoreCase = true)) ID_ONETIME else ID_REPEATING
+        showToast(context, title, message)
     }
+
+    private fun showToast(context: Context, title: String, message: String?) {
+        Toast.makeText(context, "$title : $message", Toast.LENGTH_LONG).show()
+    }
+
 }
