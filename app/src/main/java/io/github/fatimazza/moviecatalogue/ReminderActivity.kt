305e828ce4c5bff452d6c9b0e54c8eebefccd8c4
Fatima Az <azzahro.fatima@gmail.com>
2019-11-11 07:26:04 +0700

    Daily Reminder - Set Switch Change Listener

--- app/src/main/java/io/github/fatimazza/moviecatalogue/ReminderActivity.kt
+++ app/src/main/java/io/github/fatimazza/moviecatalogue/ReminderActivity.kt
@@ -1,12 +1,29 @@
 package io.github.fatimazza.moviecatalogue
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_reminder.*
 
 class ReminderActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reminder)
+
+        release_switch.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+
+            } else {
+
+            }
+        }
+
+        daily_switch.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+
+            } else {
+
+            }
+        }
     }
 }
