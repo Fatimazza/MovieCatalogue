7148799bbb7ab880ca828cabc1a753b6fc3946ed
Fatima Az <azzahro.fatima@gmail.com>
2019-11-11 07:45:07 +0700

    Daily Reminder - Toggle Daily Reminder in Reminder Activity

--- app/src/main/java/io/github/fatimazza/moviecatalogue/ReminderActivity.kt
+++ app/src/main/java/io/github/fatimazza/moviecatalogue/ReminderActivity.kt
@@ -2,6 +2,7 @@ package io.github.fatimazza.moviecatalogue
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import io.github.fatimazza.moviecatalogue.receiver.DailyReminderAlarmReceiver
 import kotlinx.android.synthetic.main.activity_reminder.*
 
 class ReminderActivity : AppCompatActivity() {
@@ -9,6 +10,8 @@ class ReminderActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reminder)
+
+        val alarmReceiver = DailyReminderAlarmReceiver()
 
         release_switch.setOnCheckedChangeListener { _, isChecked ->
             if (isChecked) {
@@ -20,9 +23,13 @@ class ReminderActivity : AppCompatActivity() {
 
         daily_switch.setOnCheckedChangeListener { _, isChecked ->
             if (isChecked) {
-
+                alarmReceiver.startDailyAlarm(
+                    this,
+                    getString(R.string.reminder_daily_notif_title),
+                    getString(R.string.reminder_daily_notif_desc)
+                )
             } else {
-
+                alarmReceiver.stopDailyAlarm()
             }
         }
     }
