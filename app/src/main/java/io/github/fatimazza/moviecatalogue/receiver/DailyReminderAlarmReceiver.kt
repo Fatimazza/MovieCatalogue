5843f29d169a22e7c32e503fc783a5638cec6573
Fatima Az <azzahro.fatima@gmail.com>
2019-11-11 07:32:00 +0700

    Daily Reminder - Change onReceive function in Alarm Receiver

--- app/src/main/java/io/github/fatimazza/moviecatalogue/receiver/DailyReminderAlarmReceiver.kt
+++ app/src/main/java/io/github/fatimazza/moviecatalogue/receiver/DailyReminderAlarmReceiver.kt
@@ -3,34 +3,31 @@ package io.github.fatimazza.moviecatalogue.receiver
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.widget.Toast
 
 class DailyReminderAlarmReceiver : BroadcastReceiver() {
 
     companion object {
-        const val TYPE_ONE_TIME = "OneTimeAlarm"
-        const val TYPE_REPEATING = "RepeatingAlarm"
+        const val EXTRA_TITLE = "title"
         const val EXTRA_MESSAGE = "message"
         const val EXTRA_TYPE = "type"
-        // Siapkan 2 id untuk 2 macam alarm, onetime dan repeating
-        private const val ID_ONETIME = 100
-        private const val ID_REPEATING = 101
+
+        private const val ID_DAILY = 100
+        private const val ID_RELEASE = 101
     }
 
     override fun onReceive(context: Context, intent: Intent) {
         // This method is called when the BroadcastReceiver is receiving an Intent broadcast.
 
-        val type = intent.getStringExtra(EXTRA_TYPE)
+        val type = intent.getIntExtra(EXTRA_TYPE, 0)
+        val title = intent.getStringExtra(EXTRA_TITLE)
         val message = intent.getStringExtra(EXTRA_MESSAGE)
-        val title =
-            if (type.equals(TYPE_ONE_TIME, ignoreCase = true)) TYPE_ONE_TIME else TYPE_REPEATING
-        val notifId =
-            if (type.equals(TYPE_ONE_TIME, ignoreCase = true)) ID_ONETIME else ID_REPEATING
-        showToast(context, title, message)
-    }
 
-    private fun showToast(context: Context, title: String, message: String?) {
-        Toast.makeText(context, "$title : $message", Toast.LENGTH_LONG).show()
+        when (type) {
+            ID_DAILY -> {
+            }
+            ID_RELEASE -> {
+            }
+        }
     }
 
 }
