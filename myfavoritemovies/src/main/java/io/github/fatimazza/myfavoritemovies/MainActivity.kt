d9d671dea6101ad98d9201c1787af957112cdf70
Fatima Az <azzahro.fatima@gmail.com>
2019-11-13 19:24:48 +0700

    Favorite Movie Module - Make Activity has Tab and show two Favorite Fragments

--- myfavoritemovies/src/main/java/io/github/fatimazza/myfavoritemovies/MainActivity.kt
+++ myfavoritemovies/src/main/java/io/github/fatimazza/myfavoritemovies/MainActivity.kt
@@ -1,12 +1,28 @@
 package io.github.fatimazza.myfavoritemovies
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+
+    private val favTabLayout: TabLayout
+        get() = tab_favorite
+
+    private val favViewPager: ViewPager
+        get() = vp_favorite
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initPageAdapter()
+    }
+
+    private fun initPageAdapter() {
+        favViewPager.adapter = FavoritesPagerAdapter(supportFragmentManager, this)
+        favTabLayout.setupWithViewPager(favViewPager)
     }
 }
