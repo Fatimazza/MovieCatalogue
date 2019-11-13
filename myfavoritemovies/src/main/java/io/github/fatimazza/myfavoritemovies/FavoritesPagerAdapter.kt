0cb9cce539ad6f1afb81f500545bd28fdeda8270
Fatima Az <azzahro.fatima@gmail.com>
2019-11-13 19:23:38 +0700

    Favorite Movie Module - Add Pager Adapter

---
+++ myfavoritemovies/src/main/java/io/github/fatimazza/myfavoritemovies/FavoritesPagerAdapter.kt
@@ -0,0 +1,35 @@
+package io.github.fatimazza.myfavoritemovies
+
+import android.content.Context
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+class FavoritesPagerAdapter(fm: FragmentManager, context: Context) :
+    FragmentPagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+
+    private val numberOfTab: Int = 2
+    private var ctx: Context = context
+
+    override fun getItem(position: Int): Fragment {
+        return when (position) {
+            0 -> FavoriteMovieFragment()
+            else -> {
+                return FavoriteTelevisionFragment()
+            }
+        }
+    }
+
+    override fun getCount(): Int {
+        return numberOfTab
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return when (position) {
+            0 -> ctx.resources.getString(R.string.menu_movie)
+            else -> {
+                ctx.resources.getString(R.string.menu_tv_show)
+            }
+        }
+    }
+}
