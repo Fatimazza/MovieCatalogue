a3a17f91defcfa7605a190fe7babb2b4d71884bb
Fatima Az <azzahro.fatima@gmail.com>
2019-11-13 07:45:46 +0700

    Favorite Content Provider - Add Insert, Get, Delete Cursor in Database DAO

--- app/src/main/java/io/github/fatimazza/moviecatalogue/database/FavoriteDatabaseDao.kt
+++ app/src/main/java/io/github/fatimazza/moviecatalogue/database/FavoriteDatabaseDao.kt
@@ -1,5 +1,6 @@
 package io.github.fatimazza.moviecatalogue.database
 
+import android.database.Cursor
 import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
@@ -12,7 +13,13 @@ interface FavoriteDatabaseDao {
     fun insertMovie(movie: FavoriteMovie)
 
     @Insert
+    fun insertMovieCursor(movie: FavoriteMovie): Long
+
+    @Insert
     fun insetTvShow(tv: FavoriteTv)
+
+    @Insert
+    fun insertTvShowCursor(tv: FavoriteTv): Long
 
     @Query("DELETE FROM fav_movie_table WHERE movieId = :key")
     fun deleteFavMovie(key: Long)
@@ -26,14 +33,26 @@ interface FavoriteDatabaseDao {
     @Query("SELECT * FROM fav_movie_table ORDER BY favMovieId DESC")
     fun getAllMoviesForWidget(): List<FavoriteMovie>
 
+    @Query("SELECT * FROM fav_movie_table ORDER BY favMovieId DESC")
+    fun getAllMoviesCursor(): Cursor
+
     @Query("SELECT * FROM fav_tv_table ORDER BY favTvId DESC")
     fun getAllTvShows(): LiveData<List<FavoriteTv>>
+
+    @Query("SELECT * FROM fav_tv_table ORDER BY favTvId DESC")
+    fun getAllTvShowsCursor(): Cursor
 
     //get a specific movie based on its key
     @Query("SELECT * from fav_movie_table WHERE movieId = :key")
     fun getFavMovie(key: Long): LiveData<FavoriteMovie>
 
+    @Query("SELECT * from fav_movie_table WHERE movieId = :key")
+    fun getFavMovieCursor(key: Long): Cursor
+
     @Query("SELECT * from fav_tv_table WHERE tvId = :key")
     fun getFavTvShow(key: Long): LiveData<FavoriteTv>
+
+    @Query("SELECT * from fav_tv_table WHERE tvId = :key")
+    fun getFavTvShowCursor(key: Long): Cursor
 }
 
