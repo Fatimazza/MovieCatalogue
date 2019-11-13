18972ccc97bfc71083fc2620e24ffcd19de93c39
Fatima Az <azzahro.fatima@gmail.com>
2019-11-13 23:06:12 +0700

    Favorite Movie Module - Change Fragments

--- myfavoritemovies/src/main/java/io/github/fatimazza/myfavoritemovies/movies/FavoriteMovieFragment.kt
+++ myfavoritemovies/src/main/java/io/github/fatimazza/myfavoritemovies/movies/FavoriteMovieFragment.kt
@@ -5,13 +5,29 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import io.github.fatimazza.myfavoritemovies.FavoriteViewModel
 import io.github.fatimazza.myfavoritemovies.R
+import io.github.fatimazza.myfavoritemovies.entity.FavoriteMovie
+import kotlinx.android.synthetic.main.fragment_favorite_movie.*
 
-/**
- * A simple [Fragment] subclass.
- */
-class FavoriteMovieFragment : Fragment() {
+
+class FavoriteMovieFragment : Fragment(), FavoriteMovieAdapter.OnItemClickCallback {
+
+    private val listFavMovie: RecyclerView
+        get() = rv_fav_movie
+
+    private val tvFavMovieEmpty: TextView
+        get() = tv_fav_movie_empty
+
+    private lateinit var listFavMovieAdapter: FavoriteMovieAdapter
+
+    private lateinit var favMovieViewModel: FavoriteViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,4 +37,45 @@ class FavoriteMovieFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_favorite_movie, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initFavoriteMovieViewModel()
+        setupListFavoriteMovieAdapter()
+        setItemClickListener()
+        fetchFavoriteMovieData()
+    }
+
+    private fun initFavoriteMovieViewModel() {
+        favMovieViewModel = ViewModelProvider(this).get(FavoriteViewModel::class.java)
+    }
+
+    private fun setupListFavoriteMovieAdapter() {
+        listFavMovie.layoutManager = LinearLayoutManager(requireContext())
+        listFavMovieAdapter = FavoriteMovieAdapter()
+        listFavMovieAdapter.notifyDataSetChanged()
+        listFavMovie.adapter = listFavMovieAdapter
+    }
+
+    private fun fetchFavoriteMovieData() {
+        favMovieViewModel.favouriteMovies.observe(this, Observer { listMovie ->
+            if (listMovie != null) {
+                listFavMovieAdapter.setData(listMovie)
+                tvFavMovieEmpty.visibility = if (listMovie.isEmpty()) View.VISIBLE else View.GONE
+            } else {
+                listFavMovieAdapter.setData(listFavMovieAdapter.getData())
+                tvFavMovieEmpty.visibility = View.VISIBLE
+            }
+        })
+    }
+
+    private fun setItemClickListener() {
+        listFavMovieAdapter.setOnItemClickCallback(this)
+    }
+
+    override fun onItemClicked(data: FavoriteMovie) {
+        /*val intentMovie = Intent(requireContext(), DetailMovieActivity::class.java).apply {
+            putExtra(DetailMovieActivity.EXTRA_MOVIE, data.movieId)
+        }
+        startActivity(intentMovie)*/
+    }
 }
--- myfavoritemovies/src/main/java/io/github/fatimazza/myfavoritemovies/television/FavoriteTelevisionFragment.kt
+++ myfavoritemovies/src/main/java/io/github/fatimazza/myfavoritemovies/television/FavoriteTelevisionFragment.kt
@@ -5,13 +5,29 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import io.github.fatimazza.myfavoritemovies.FavoriteViewModel
 import io.github.fatimazza.myfavoritemovies.R
+import io.github.fatimazza.myfavoritemovies.entity.FavoriteTv
+import kotlinx.android.synthetic.main.fragment_favorite_television.*
 
-/**
- * A simple [Fragment] subclass.
- */
-class FavoriteTelevisionFragment : Fragment() {
+
+class FavoriteTelevisionFragment : Fragment(), FavoriteTelevisionAdapter.OnItemClickCallback {
+
+    private val listFavTelevision: RecyclerView
+        get() = rv_fav_tv
+
+    private val tvFavTelevisionEmpty: TextView
+        get() = tv_fav_television_empty
+
+    private lateinit var listFavTelevisionAdapter: FavoriteTelevisionAdapter
+
+    private lateinit var favTelevisionViewModel: FavoriteViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,5 +37,46 @@ class FavoriteTelevisionFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_favorite_television, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initFavoriteTelevisionViewModel()
+        setupListFavoriteTelevisionAdapter()
+        setItemClickListener()
+        fetchFavoriteTelevisionData()
+    }
 
+    private fun initFavoriteTelevisionViewModel() {
+        favTelevisionViewModel = ViewModelProvider(this).get(FavoriteViewModel::class.java)
+    }
+
+    private fun setupListFavoriteTelevisionAdapter() {
+        listFavTelevision.layoutManager = LinearLayoutManager(requireContext())
+        listFavTelevisionAdapter = FavoriteTelevisionAdapter()
+        listFavTelevisionAdapter.notifyDataSetChanged()
+        listFavTelevision.adapter = listFavTelevisionAdapter
+    }
+
+    private fun fetchFavoriteTelevisionData() {
+        favTelevisionViewModel.favouriteTvs.observe(this, Observer { listTvShow ->
+            if (listTvShow != null) {
+                listFavTelevisionAdapter.setData(listTvShow)
+                tvFavTelevisionEmpty.visibility =
+                    if (listTvShow.isEmpty()) View.VISIBLE else View.GONE
+            } else {
+                listFavTelevisionAdapter.setData(listFavTelevisionAdapter.getData())
+                tvFavTelevisionEmpty.visibility = View.VISIBLE
+            }
+        })
+    }
+
+    private fun setItemClickListener() {
+        listFavTelevisionAdapter.setOnItemClickCallback(this)
+    }
+
+    override fun onItemClicked(data: FavoriteTv) {
+        /*val intentMovie = Intent(requireContext(), DetailMovieActivity::class.java).apply {
+            putExtra(DetailMovieActivity.EXTRA_TELEVISION, data.tvId)
+        }
+        startActivity(intentMovie)*/
+    }
 }
